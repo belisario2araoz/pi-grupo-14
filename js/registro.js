e60d0999efd2ac9d55805988bc4277150361bccf
justomuina <jmuina@udesa.edu.ar>
2025-11-21 14:38:24 -0300

    avanzando y actualizando devuelta el registro

--- js/registro.js
+++ js/registro.js
@@ -20,3 +20,32 @@ formulario.addEventListener('submit', function(event){
         formulario.submit();
 }});
 
+function registroUsuario(e) {
+    e.preventDefault();
+
+
+
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
+    const repeatPassword = document.querySelector('#repeatPassword').value
+    const terms = document.querySelector('#acep').checked
+
+        console.log(email)
+
+    if(password !== repeatPassword){
+        alert('las contrasenias no son iguales')
+        return
+    }
+    if(!terms){
+        alert('Debes aceptar los terminos y condiciones')
+        return
+    }
+
+    const nuevoUser = {email, password}
+    localStorage.setItem("registeredUser", JSON.stringify(nuevoUser))
+
+    localStorage.setItem('loggedUSer', JSON.stringify({email}))
+
+    window.location.href = 'index.html'
+
+}
