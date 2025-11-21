8246ee941d42f10ee9e508209151e90848cca22a
Luca Carpanetto <lcarpanetto@udesa.edu.ar>
2025-11-21 08:30:41 -0300

    fix: resolver conflictos Js productos

--- js/producto.js
+++ js/producto.js
@@ -1,59 +1,73 @@
 let queryString = location.search;
-console.log(queryString);
-let urlParams = new URLSearchParams(queryString);
-let productoId = urlParams.get("id");
-console.log(productoId);
+let productoId = queryString.split("=")[1];
 
-let comentarios = document.querySelector('.comentarios')
+let seccion = document.querySelector(".detalleproducto");
 
 fetch(`https://dummyjson.com/products/${productoId}`)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
-        let seccion = document.querySelector(".detalleproducto")
-        seccion.innerHTML =
-            `<h2 class="tituloPro">${data.title}</h2>
-            <h3 class="marcaPro">${data.brand}</h3>
-            <img src=${data.thumbnail}>
-            <p class="descPro">${data.description}</p>
-            <p class="precioPro">${data.price}</p>
-            <p class="stockPro">${data.stock}</p>
-                    <a href="./categorias.html?category=${data.category}">
-                                <p class="catPro"> ${data.category.replaceAll("-", " ")}</p>
-                    </a>`
+        seccion.innerHTML = `
+            <div class="producto-flex">
+                <div class="columna-izq">
+                    <img src="${data.thumbnail}" alt="${data.title}">
+                </div>
+                <div class="columna-der">
+                    <h2 class="tituloPro">${data.title}</h2>
+                    <h3 class="marcaPro">${data.brand}</h3>
+                    <p class="precioPro">$${data.price}</p>
+                    <p class="descPro">${data.description}</p>
+                    <p class="stockPro">Stock: ${data.stock}</p>
+                    <a href="./categorias.html?category=${data.category}" class="boton-cat">
+                        Ver categoría: ${data.category.replaceAll("-", " ")}
+                    </a>
+                </div>
+            </div>
+            
+            <div class="seccion-comentarios">
+                <h3>Opiniones de usuarios</h3>
+                <div class="lista-comentarios"></div>
+            </div>
+        `;
 
+        let contenedorComentarios = document.querySelector('.lista-comentarios');
+        
         for (let i = 0; i < data.reviews.length; i++) {
-
-            comentarios.innerHTML +=
-                `<section class="comment">
-                <p class="rating">${data.reviews[i].rating}</p>
-                <p class="user">${data.reviews[i].reviewerName}</p>
-                <p class="comment">${data.reviews[i].comment}</p>
-                <p class="date">${data.reviews[i].date}</p>
-            </section>`
+            contenedorComentarios.innerHTML += `
+                <article class="comment-card">
+                    <p class="rating">⭐ ${data.reviews[i].rating}</p>
+                    <p class="comment">"${data.reviews[i].comment}"</p>
+                    <p class="user">- ${data.reviews[i].reviewerName}</p>
+                </article>
+            `;
         }
     })
+    .catch(function(error){
+        console.log("Error: " + error);
+    });
+
 let formulario = document.querySelector(".buscadorForm");
 let campoBusqueda = document.querySelector('.buscadorInput');
 let errorBusqueda = document.querySelector('.invalid-feedback-searchInput');
 
-formulario.addEventListener('submit', function (event) {
-    event.preventDefault();
-    let valid = true;
+if(formulario){
+    formulario.addEventListener('submit', function (event) {
+        event.preventDefault();
+        let valid = true;
 
-    if (campoBusqueda.value === "") {
-        errorBusqueda.innerText = 'Por favor complete el campo de búsqueda';
-        errorBusqueda.style.display = 'block';
-        valid = false;
-    }
-    else if (campoBusqueda.value.length < 3) {
-        errorBusqueda.innerText = 'El término de búsqueda debe tener al menos 3 caracteres';
-        errorBusqueda.style.display = 'block';
-        valid = false;
-    }
-    if (valid) {
-        formulario.submit();
-    }
-});
+        if (campoBusqueda.value === "") {
+            errorBusqueda.innerText = 'Por favor complete el campo de búsqueda';
+            errorBusqueda.style.display = 'block';
+            valid = false;
+        }
+        else if (campoBusqueda.value.length < 3) {
+            errorBusqueda.innerText = 'El término de búsqueda debe tener al menos 3 caracteres';
+            errorBusqueda.style.display = 'block';
+            valid = false;
+        }
+        if (valid) {
+            formulario.submit();
+        }
+    });
+}
